42a951a8cf02be2cbd066af31c968dd319459539
Rewati123 <rewatiraman130@gmail.com>
2025-02-27 17:22:50 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Lerniningcenter from "../components/Lerniningcenter"
 import CorePrograms from "components/CorePrograms";
-import Comprehensive from "components/Comprehensive";
+// import Comprehensive from "components/Comprehensive";
 import EmpoweringWomen from "components/EmpoweringWomen";
 import Partners from "components/Partners";
 import Contact from "components/Contact";
@@ -14,17 +14,12 @@ import Contact from "components/Contact";
 export default function Home() {
  
 
-
-
- 
-
-
   return (
     <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
         <Lerniningcenter/>
         <CorePrograms/>
 
-        <Comprehensive/>
+        {/* <Comprehensive/> */}
         <EmpoweringWomen/>
 
         <Partners />
